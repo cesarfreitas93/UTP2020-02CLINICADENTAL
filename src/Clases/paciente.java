d8382b6cc2ae9b18f5b3712056289f70f4969111
lufravapekeanu <netsource_red@outlook.com>
2020-11-04 20:04:41 -0500

    Añadi la libreria para añadir a sql

--- src/Clases/paciente.java
+++ src/Clases/paciente.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases;
 
-/**
- *
- * @author lufra
- */
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class paciente {
+    bdconexion con;// inicializa la variable conexion
+    
+    //Constructor de la clase paciente
+    public paciente()
+    {
+        con = new bdconexion(); //instancia la clase bdconexion
+    }
+    
+     //constructor explicito para agregar un nuevo registro a la base de datos
+    
+      public void nuevoPaciente(String idPaciente, String DNI, String nombres, String apellidos, String direccion, String departamento, String provincia,String Distrito, String telefono1, String telefono2, String edad)
+    {
+        try
+        {
+            PreparedStatement pstm=(PreparedStatement)
+            con.getConnection().prepareStatement("insert into " + " paciente(idPaciente, dui, nombres, apellidos, direccion, departamento, provincia, Distrito, telefono1, telefono2, edad)" + "values (?,?,?,?,?,?,?,?,?,?,?)");
+            
+            pstm.setString(1, idPaciente);
+            pstm.setString(2, DNI);
+            pstm.setString(3, nombres);
+            pstm.setString(4, apellidos);
+            pstm.setString(5, direccion);
+            pstm.setString(6, departamento);
+            pstm.setString(8, Distrito);
+            pstm.setString(9, Distrito);
+            pstm.setString(10, telefono1);
+            pstm.setString(11, telefono2);
+            pstm.setString(12, edad);            
+            
+            pstm.execute();
+            pstm.close();
+        }
+        catch(SQLException e)
+        {
+            System.out.print(e);
+        }
+    }
+    
     
 }
